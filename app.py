895050bce4ad5025320c120b6af0c762f8f2e377
Guilherme Luiz Martins Santos <GuilhermeL@BFDIAS.LOCAL>
2026-03-06 08:33:13 -0300

    desmantelar

--- app.py
+++ app.py
@@ -416,7 +416,7 @@ for i, operador in enumerate(operadores):
             st.success("Alterações salvas")
             st.rerun()
 
-        if st.button("Abrir essa merda"):
+        if st.button("Abrir essa merda", key=f"abrir_pdf_{i}"):
             mostrar_pdf("desenhos/peca123.pdf")
 
 else:
